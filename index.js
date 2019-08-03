e2ea4041a7776862794a356a4531fb0b361fc13f
kanestapler <kanestapler@gmail.com>
2019-08-02 23:42:28 -0700

    changing pin out

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const board = new five.Board({
 board.on('ready', () => {
   // Create an Led on pin 7 (GPIO4) on P1 and strobe it on/off
   // Optionally set the speed; defaults to 100ms
-  new five.Led('P1-7').strobe()
+  new five.Led('GPIO7').strobe()
 })
