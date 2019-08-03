374228a3990a3e711d8e3f84cdb6061a920c22ca
kanestapler <kanestapler@gmail.com>
2019-08-03 16:54:14 -0700

    Testing led array

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const Raspi = require('raspi-io').RaspiIO
 const five = require('johnny-five')
+const pixel = require('node-pixel')
+
 const board = new five.Board({
   io: new Raspi(),
 })
 
+let strip = null
+
 board.on('ready', () => {
-  // Create an Led on pin 7 (GPIO4) on P1 and strobe it on/off
-  // Optionally set the speed; defaults to 100ms
-  new five.Led('GPIO7').strobe()
+  //   new five.Led('GPIO7').strobe()
+  strip = new pixel.Strip({
+    board: this,
+    controller: 'FIRMATA',
+    strips: [{ pin: 'GPIO7', length: 144 }],
+    gamma: 2.8,
+  })
+
+  // Just like DOM-ready for web developers.
+  strip.on('ready', function() {
+    // Set the entire strip to pink.
+    strip.color('#903')
+
+    // Send instructions to NeoPixel.
+    strip.show()
+  })
 })
